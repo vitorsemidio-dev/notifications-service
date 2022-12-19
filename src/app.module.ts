23546d1f9a8ac51777ce117f70fb3cb529e9251d
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-12-19 11:16:04 -0300

    feat(messaging): create module, setup kafka and topics

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { MessagingModule } from '@infra/messaging/messaging.module';
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { DatabaseModule } from './infra/database/database.module';
 import { HttpModule } from './infra/http/http.module';
 
 @Module({
-  imports: [DatabaseModule, HttpModule],
+  imports: [DatabaseModule, HttpModule, MessagingModule],
   providers: [
     {
       provide: APP_PIPE,
--- src/infra/shared/shared.module.ts
+++ src/infra/shared/shared.module.ts
@@ -1,8 +1,9 @@
+import { ConfigModule } from '@infra/config/config.module';
 import { DatabaseModule } from '@infra/database/database.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [DatabaseModule],
-  exports: [DatabaseModule],
+  imports: [DatabaseModule, ConfigModule],
+  exports: [DatabaseModule, ConfigModule],
 })
 export class SharedModule {}
